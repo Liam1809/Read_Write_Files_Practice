9e85edf7604c65c33b3c9bc63e6816b1b9b7d598
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-14 22:42:24 +0700

    Iterating through lines

--- Knowledge.py
+++ Knowledge.py
@@ -14,3 +14,26 @@
 #   cool_contents = cool_doc.read()
 # print(cool_contents)
 # This opens a file object called cool_doc and creates a new indented block where you can read the contents of the opened file. We then read the contents of the file cool_doc using cool_doc.read() and save the resulting string into the variable cool_contents. Then we print cool_contents, which outputs the statement Wowsers!.
+
+# Alternatively, one could write the code as follows.
+
+# text_file = open('welcome.txt')
+# text_data = text_file.read()
+# print(text_data)
+# text_file.close()
+
+# Iterating Through Lines
+# When we read a file, we might want to grab the whole document in a single string, like .read() would return. But what if we wanted to store each line in a variable? We can use the .readlines() function to read a text file line by line instead of having the whole thing. Suppose we have a file:
+
+# keats_sonnet.txt
+
+# To one who has been long in city pent,
+# ’Tis very sweet to look into the fair
+# And open face of heaven,—to breathe a prayer
+# Full in the smile of the blue firmament.
+# script.py
+
+# with open('keats_sonnet.txt') as keats_sonnet:
+#   for line in keats_sonnet.readlines():
+#     print(line)
+# The above script creates a temporary file object called keats_sonnet that points to the file keats_sonnet.txt. It then iterates over each line in the document and prints the entire file out.
--- Practice.py
+++ Practice.py
@@ -1,3 +1,16 @@
 with open("welcome.txt") as text_file:
   text_data = text_file.read()
   print(text_data)
+  
+with open("welcome.txt") as text_file:
+  text_data_1 = text_file.readlines()
+  print(text_data_1)
+  
+# work better with lengthy text by approaching one line at a time
+with open("how_many_lines.txt") as lines_doc:
+  for line in lines_doc.readlines():
+    print(line)
+# work faster with short text 
+with open("how_many_lines.txt") as lines_doc:
+    for line in lines_doc:
+        print(line)
