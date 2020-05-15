75dc4e0621b8a26786e50d991acb808aea46e465
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-15 20:18:32 +0700

    Reading Different Types of CSV Files

--- CSV_Practice.py
+++ CSV_Practice.py
@@ -1,4 +1,5 @@
 import csv
+# Reading a CSV File
 with open("cool_csv.csv") as cool_csv_file:
   # for row in cool_csv_file:
   #   print(row.strip())
@@ -6,3 +7,8 @@ with open("cool_csv.csv") as cool_csv_file:
   # print(list(cool_csv_dict))
   for row in cool_csv_dict:
     print(row['Cool Fact'])
+# Reading Different Types of CSV Files
+with open("books.csv", newline = '') as books_csv:
+  books_reader = csv.DictReader(books_csv, delimiter = "@")
+  isbn_list = [row['ISBN'] for row in books_reader]
+  print(isbn_list)
