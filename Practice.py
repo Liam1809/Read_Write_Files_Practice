b8844717af3506a3f9405971286fbc63d49bcd76
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-15 15:08:20 +0700

    Reading a Line

--- Practice.py
+++ Practice.py
@@ -1,3 +1,4 @@
+# Reading a File
 with open("welcome.txt") as text_file:
   text_data = text_file.read()
   print(text_data)
@@ -6,6 +7,7 @@ with open("welcome.txt") as text_file:
   text_data_1 = text_file.readlines()
   print(text_data_1)
   
+# Iterating Through Lines
 # work better with lengthy text by approaching one line at a time
 with open("how_many_lines.txt") as lines_doc:
   for line in lines_doc.readlines():
@@ -14,3 +16,11 @@ with open("how_many_lines.txt") as lines_doc:
 with open("how_many_lines.txt") as lines_doc:
     for line in lines_doc:
         print(line)
+        
+        
+# Reading a Line      
+with open("just_the_first.txt") as first_line_doc:
+  first_line = first_line_doc.readline()
+  first_line1 = first_line_doc.readline()
+  print(first_line)
+  print(first_line1)
