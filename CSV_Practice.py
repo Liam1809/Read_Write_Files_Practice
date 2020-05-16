e6fd1b5b0c93c5b7d5462d5bf123c1275f1f114f
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-16 17:09:03 +0700

    update

--- CSV_Practice.py
+++ CSV_Practice.py
@@ -1,14 +1,16 @@
 import csv
-# Reading a CSV File
-with open("cool_csv.csv") as cool_csv_file:
-  # for row in cool_csv_file:
-  #   print(row.strip())
-  cool_csv_dict = csv.DictReader(cool_csv_file)
-  # print(list(cool_csv_dict))
-  for row in cool_csv_dict:
-    print(row['Cool Fact'])
-# Reading Different Types of CSV Files
+# # Reading a CSV File
+# with open("cool_csv.csv") as cool_csv_file:
+#   # for row in cool_csv_file:
+#   #   print(row.strip())
+#   cool_csv_dict = csv.DictReader(cool_csv_file)
+#   # print(list(cool_csv_dict))
+#   for row in cool_csv_dict:
+#     print(row['Cool Fact'])
+# # Reading Different Types of CSV Files
+
 with open("books.csv", newline = '') as books_csv:
+#   print(books_csv.read())
   books_reader = csv.DictReader(books_csv, delimiter = "@")
   isbn_list = [row['ISBN'] for row in books_reader]
   print(isbn_list)
